4488c5bab47fae6b12ff5c6c7a42248519a41b69
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-05-25 23:49:33 +0700

    fix (main.jsx) : delete strick mode

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,6 @@ import router from './Router';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
 )
 
