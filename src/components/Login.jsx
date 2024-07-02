7689c8161e5fb74165f9f71f4272a6987d890b9f
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-07-03 02:56:29 +0700

    fix (login) : change api BE

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,19 @@
-import React, { Fragment, useState } from 'react';
-// import Auth from '../api/auth/Auth';
-import { message } from 'antd';
-import Auth from '../api/auth/Auth';
-import Loading from './Loading';
-import Utils from '../utils/Utils';
+import React, { Fragment, useState } from "react";
+import { message } from "antd";
+import Loading from "./Loading";
+import Utils from "../utils/Utils";
+import axios from "axios";
 
 const Login = () => {
     const { loading, showLoading, hideLoading } = Utils();
     const [data, setData] = useState({
-        email: '',
-        password: ''
+        username: "",
+        password: "",
     });
 
     const [error, setError] = useState({
-        email: '',
-        password: ''
+        username: "",
+        password: "",
     });
 
     const [showPassword, setShowPassword] = useState(false);
@@ -22,25 +21,25 @@ const Login = () => {
     const onChangeInput = (e) => {
         setData({
             ...data,
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
         });
 
         setError({
             ...error,
-            [e.target.name]: ''
+            [e.target.name]: "",
         });
     };
 
     const validateInput = () => {
         let isValid = true;
         const newError = {};
-
-        if (data.email.trim() === '') {
-            newError.email = 'Email is required';
+        console.log(data.username);
+        if (data.username.trim() === "" || data.username.trim().length == 0) {
+            newError.username = "Email is required";
             isValid = false;
         }
-        if (data.password.trim() === '') {
-            newError.password = 'Password is required';
+        if (data.password.trim() === "") {
+            newError.password = "Password is required";
             isValid = false;
         }
 
@@ -49,19 +48,28 @@ const Login = () => {
         return isValid;
     };
 
-    const submitForm = async (e) => {
+    const submitForm = (e) => {
         e.preventDefault();
         if (validateInput()) {
             showLoading();
-            const { data: user, error } = await Auth.login(data.email.trim(), data.password);
-            if (error) {
-                message.error(error)
-                hideLoading();
-            } else {
-                hideLoading();
-                localStorage.setItem('token', JSON.stringify(user) )
-                window.location.href = "/cms/dashboard";
-            }
+            axios
+                .post("http://localhost:3001/api/auth/login", data)
+                .then((response) => {
+                    const res = response.data;          //TODO check is admin or guru
+                    if (res.statusCode === 200) {
+                        message.success(res.message);
+                        localStorage.setItem("token", res.data.token);
+                        window.location.href = "/cms/dashboard";
+                    }
+                })
+                .catch((error) => {
+                    const err = error.response.data;
+                    console.log(err);
+                    message.error(err.message);
+                })
+                .finally(() => {
+                    hideLoading();
+                });
         }
     };
 
@@ -77,45 +85,97 @@ const Login = () => {
                             <form onSubmit={submitForm}>
                                 <div className="space-y-4 md:space-y-6">
                                     <div>
-                                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Username</label>
+                                        <label
+                                            htmlFor="username"
+                                            className="block mb-2 text-sm font-medium text-gray-900"
+                                        >
+                                            Username
+                                        </label>
                                         <input
                                             onChange={(e) => onChangeInput(e)}
                                             type="text"
-                                            name="email"
-                                            id="email"
+                                            name="username"
+                                            id="username"
                                             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                                            placeholder="username" />
-                                        {error.email && <p className="text-red-500 text-xs">{error.email}</p>}
+                                            placeholder="username"
+                                        />
+                                        {error.username && (
+                                            <p className="text-red-500 text-xs">
+                                                {error.username}
+                                            </p>
+                                        )}
                                     </div>
                                     <div>
-                                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                                        <label
+                                            htmlFor="password"
+                                            className="block mb-2 text-sm font-medium text-gray-900"
+                                        >
+                                            Password
+                                        </label>
                                         <input
                                             onChange={(e) => onChangeInput(e)}
-                                            type={showPassword ? "text" : "password"}
+                                            type={
+                                                showPassword
+                                                    ? "text"
+                                                    : "password"
+                                            }
                                             name="password"
                                             id="password"
                                             placeholder="••••••••"
-                                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " />
-                                        {error.password && <p className="text-red-500 text-xs">{error.password}</p>}
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                                        />
+                                        {error.password && (
+                                            <p className="text-red-500 text-xs">
+                                                {error.password}
+                                            </p>
+                                        )}
                                     </div>
                                     <div className="flex items-center justify-between">
                                         <div className="flex items-start">
                                             <div className="flex items-center h-5">
                                                 <input
-                                                    onChange={() => setShowPassword(!showPassword)}
-                                                    id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" />
+                                                    onChange={() =>
+                                                        setShowPassword(
+                                                            !showPassword
+                                                        )
+                                                    }
+                                                    id="remember"
+                                                    aria-describedby="remember"
+                                                    type="checkbox"
+                                                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                                                />
                                             </div>
                                             <div className="ml-3 text-sm">
-                                                <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Show password</label>
+                                                <label
+                                                    htmlFor="remember"
+                                                    className="text-gray-500 dark:text-gray-300"
+                                                >
+                                                    Show password
+                                                </label>
                                             </div>
                                         </div>
-                                        <a href="#" className="text-sm font-medium text-primary-600 hover:underline">Forgot password?</a>
+                                        <a
+                                            href="#"
+                                            className="text-sm font-medium text-primary-600 hover:underline"
+                                        >
+                                            Forgot password?
+                                        </a>
                                     </div>
                                     <button
                                         onClick={(e) => submitForm(e)}
-                                        type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                                        type="submit"
+                                        className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                                    >
+                                        Sign in
+                                    </button>
                                     <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                        Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline">Sign up</a>
+                                        Don’t have an account yet?{" "}
+                                        <a
+                                            href="#"
+                                            className="font-medium text-primary-600 hover:underline"
+                                        >
+                                            Sign up
+                                        </a>
                                     </p>
                                 </div>
                             </form>
