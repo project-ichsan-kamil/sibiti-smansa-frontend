cdee5c3c4b511338555982b51c08c0ea02958e47
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-09-15 02:15:05 +0700

    feat (login) : add UI login user

---
+++ src/assets/UserImage.jsx
@@ -0,0 +1,9 @@
+import welcome from './img/welcome.webp';
+import smansa from './img/smansa.png';
+
+const image = {
+  welcome,
+  smansa
+};
+
+export default image;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    plugins: [],
+    theme: {
+        extend: {
+            keyframes: {
+                fadeIn: {
+                    "0%": { opacity: "0" },
+                    "100%": { opacity: "1" },
+                },
+                slideIn: {
+                    "0%": { transform: "translateY(20px)", opacity: "0" },
+                    "100%": { transform: "translateY(0)", opacity: "1" },
+                },
+            },
+            animation: {
+                "fade-in": "fadeIn 1s ease-out",
+                "slide-in": "slideIn 0.5s ease-out",
+            },
+            backgroundColor: {
+                active: "#2881CF",
+                default: "#F7FBFF",
+                disabled: "#144B84",
+                hover: "#3A98E8"
+            },
+            textColor: {
+                active: "#2881CF",
+                default: "#F7FBFF",
+                disabled: "#144B84",
+                hover: "#3A98E8"
+            },
+            borderColor: {
+                active: "#2881CF",
+                default: "#F7FBFF",
+                disabled: "#144B84",
+                hover: "#3A98E8"
+            },
+        },
+    },
+};
